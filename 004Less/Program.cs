c4647d74dbb764f9eee3d09a617b76fda4f9bab6
Dmitryi <dimon_smirnov81@mail.ru>
2022-08-14 19:08:53 +0300

    Switch branch 002

--- 004Less/Program.cs
+++ 004Less/Program.cs
@@ -51,7 +51,7 @@ for (int i = 0; i < n - 1; i++)
 }
 Console.WriteLine("Конечный массив: [" + string.Join(", ", array) + "]");
 */
-
+/*
 string[] array = new string[5];
 for (int i = 0; i < 5; i++)
 {
@@ -74,3 +74,32 @@ for (int i = 0; i < 4; i++)
 }
 System.Console.WriteLine();
 Console.WriteLine("Конечный массив: [" + string.Join(", ", array) + "]");
+*/
+int n = Convert.ToInt32(Console.ReadLine());
+int[] a = new int[n];
+int[] b = new int[n];
+for (int i = 0; i < n; i++)
+{
+    a[i] = Convert.ToInt32(Console.ReadLine());
+}
+int k = Convert.ToInt32(Console.ReadLine());
+k = k % n;
+if (k > 0)
+{
+for (int i = 0; i <= k; i++)
+    b[i] = a[n - k + i];
+for (int i = 0; i < n - k; i++)
+    b[k + i] = a[i];
+for (int i = 0; i < n; i++)
+    Console.Write(b[i] + " ");
+}
+else
+{
+k = -k;
+for (int i = 0; i < k; i++)
+    b[n - k + i] = a[i];
+for (int i = 0; i < n - k; i++)
+    b[i] = a[k + i];
+for (int i = 0; i < n; i++)
+    Console.Write(b[i] + " ");
+}
